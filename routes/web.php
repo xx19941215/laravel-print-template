4b7a74687785ffac882093d25b952a3f5eff9eb2
xiaox <857635782@qq.com>
2025-11-11 09:49:28 +0800

    feat(route): support array format for HTTP methods in route configuration

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ $routes = config('print_template.routes', []);
 
 // 注册路由
 foreach ($routes as $route) {
-    Route::match([$route['method']], $route['uri'], $route['action'])
+    // 确保 method 是数组形式，以支持字符串和数组两种配置方式
+    $methods = is_array($route['method']) ? $route['method'] : [$route['method']];
+    Route::match($methods, $route['uri'], $route['action'])
         ->middleware($route['middlewares'] ?? [])
         ->name($route['name'] ?? '');
 }
